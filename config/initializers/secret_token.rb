d3e81a75120593047f281fd6ba1c43920a0844fa
' <'>
2014-05-01 13:03:23 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jaye40weekexam::Application.config.secret_token = 'b44375e338d4c5ea6149bd4232b519151053aa5eefd91f76922830ba2339de97af1d400b15dc8e926a3f11f339b9cfed0aa2ae15fd97ff83c6bd0fccd8e3d47f'
